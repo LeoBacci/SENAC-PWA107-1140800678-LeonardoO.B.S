8afee4d3428c663ed74d612764bf881cd356b492
LeoBacci <leobaccisilva99@gmail.com>
2022-09-19 10:54:16 -0300

    Ado Projeto Aula WEB

---
+++ src/pages/Contato/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Contato() {
+    return (
+        <div>
+            <h1>Página de Contato</h1>
+            <span>Email: nossaEmpresa@nossaEmpresa.com</span>
+            <br />
+            <span>WhatsApp: (11)99666-6666</span><br />
+
+        </div>
+    )
+}
+export default Contato;
---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Sobre() {
+    return (
+        <div>
+            <h1>Bem vinda a Página Sobre</h1>
+            <span>Sobre nossa empresa</span>
+            <br />
+        </div>
+    )
+}
+export default Sobre;
